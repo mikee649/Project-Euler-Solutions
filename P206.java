bb70c1055475a41527d46cf85ef0f7aab10cfc90
Michael Rokas <michaelrokas649@gmail.com>
2019-05-21 20:53:46 -0400

    added P206

---
+++ P206.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;  
+
+class P206 {
+	public static void main(String[] args) {
+		long i = 1010101010;	//sqrt(1020304050607080900)
+
+		String regex = "1\\d2\\d3\\d4\\d5\\d6\\d7\\d8\\d9\\d0";
+
+		while(!Pattern.matches(regex, "" + i*i))
+			i += 10;	//increase by 10 becasause the final number
+						//will have to be divisible by 10
+
+		System.out.println(i);
+	}
+}
